243f1fcab437b61f4991678ab134b8f1e1c4faa4
Vijay Guttula <vijayguttula001@gmail.com>
2025-06-08 22:19:36 +0530

    Minimized Hello World file

--- Week6/HelloWorld.java
+++ Week6/HelloWorld.java
@@ -1,10 +1,7 @@
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.*;
+import jakarta.servlet.http.*;
 
 /**
  * Servlet implementation class HelloWorld
